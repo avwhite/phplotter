96cbeefc95ef17ef0492b5553f94b92dacf13ec3
Andreas VH <andreas.vinterhviid@gmail.com>
2011-10-13 20:26:47 +0200

    Update drawing on line. Add early function and class for dealing with points.
    The line drawen now somehow depends on what input is given, but there is a long way to go before it starts drawing anything usefull.

--- plot.php
+++ plot.php
@@ -2,6 +2,18 @@
 header('Content-type: image/png');
 
 include('cfunc.php');
+class Point {
+	public function __construct($x, $y) {
+		$this->x = $x;
+		$this->y = $y;
+	}
+	public $x, $y;
+}
+function tc(Point $p) {
+	$x = 500 - $p->x;
+	$y = $p->y;
+	return new Point($x, $y);
+}
 
 $mathString = $_GET['expr'];
 $f = createFunc($mathString);
@@ -13,7 +25,16 @@ $blue = imagecolorallocate($img, 0, 0, 255);
 
 imagefilledrectangle($img, 0, 0, 500, 500, $black);
 
-imageline($img, 0, 30, 50, 40, $blue);
+$lx = 0;
+$ly = $f->evalu(0);
+
+for($i = 0; $i < 100; $i += 10) {
+	$nx = $i;
+	$ny = $f->evalu($i);
+	imageline($img, $lx, $ly, $nx, $ny, $blue); 
+	$lx = $nx;
+	$ly = $ny;
+}
 
 imagepng($img);
 imagedestroy($image);
