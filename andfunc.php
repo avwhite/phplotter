4d4e5d5cb37ede28579b5010596fe6c9d0434cd7
Andreas VH <andreas.vinterhviid@gmail.com>
2011-11-07 09:16:15 +0100

    Fix error where cfunc handled everything that gave 0 as an error (Stupid dynamic typing :p)

--- andfunc.php
+++ andfunc.php
@@ -1,35 +1,35 @@
 <?php
 function add($a, $b) { 
-	if($a == null || $b == null) {
+	if($a === null || $b === null) {
 		return null;
 	} else {
 		return $a + $b; 
 	}
 }
 function sub($a, $b) {
-	if($a == null || $b == null) {
+	if($a === null || $b === null) {
 		return null;
 	} else {
 		return $a - $b; 
 	}
 }
 function mul($a, $b) {
-	if($a == null || $b == null) {
+	if($a === null || $b === null) {
 		return null;
 	} else {
 		return $a * $b; 
 	}
 }
 function div($a, $b) {
-	if($b == 0) { return null; }
-	if($a == null || $b == null) {
+	if($b === 0) { return null; }
+	if($a === null || $b === null) {
 		return null;
 	} else {
 		return $a / $b; 
 	}
 }
 function expo($a, $b) {
-	if($a == null || $b == null) {
+	if($a === null || $b === null) {
 		return null;
 	} else {
 		return pow($a, $b); 
