dbdc73657fba5fca0498a7fb9f97e63a18950c61
Andreas VH <andreas.vinterhviid@gmail.com>
2011-11-24 21:20:48 +0100

    Clear a few things up in test.php.

--- test.php
+++ test.php
@@ -4,7 +4,6 @@
 		<title>Test of cfunc</title>
 	</head>
 	<body>
-		<?php echo null / 2; ?>
 		<form method="post" action="test.php" />
 			f(x)=<input type="text" name="func" /><br />
 			xmin=<input type="text" name="xmin" />xmax=<input type="text" name="xmax" /><br />
@@ -12,7 +11,6 @@
 			<input type="submit" />
 		</form>
 		<?php
-		echo createFunc($_REQUEST['func'])->evalu(1)."<br />";
 		echo '<img src="plot.php?expr='.urlencode($_REQUEST['func']).'&xmin='.$_REQUEST['xmin'].'&xmax='.$_REQUEST['xmax'].'&ymin='.$_REQUEST['ymin'].'&ymax='.$_REQUEST['ymax'].'" />';
 		?>
 		<br />
