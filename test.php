a8f9a67d23122dfbc7f19cdafcd12577a70d2f0e
Andreas VH <andreas.vinterhviid@gmail.com>
2011-11-26 16:05:41 +0100

    Make test page far easier to use.
    Test page now retains form input after you press submit.

--- test.php
+++ test.php
@@ -4,11 +4,18 @@
 		<title>Test of cfunc</title>
 	</head>
 	<body>
+		<?php
+		$func = isset($_POST['func']) ? $_POST['func'] : '';
+		$xmax = isset($_POST['xmax']) ? $_POST['xmax'] : '';
+		$xmin = isset($_POST['xmin']) ? $_POST['xmin'] : '';
+		$ymax = isset($_POST['ymax']) ? $_POST['ymax'] : '';
+		$ymin = isset($_POST['ymin']) ? $_POST['ymin'] : '';
+		?>
 		<form method="post" action="test.php" />
-			f(x)=<input type="text" name="func" /><br />
-			xmin=<input type="text" name="xmin" />xmax=<input type="text" name="xmax" /><br />
-			ymin=<input type="text" name="ymin" />ymax=<input type="text" name="ymax" />
-			<input type="submit" />
+			f(x)=<input type="text" name="func" value="<?php echo $func; ?>"/><br />
+			xmin=<input type="text" name="xmin" value="<?php echo $xmin; ?>"/>xmax=<input type="text" name="xmax" value="<?php echo $xmax; ?>"/><br />
+			ymin=<input type="text" name="ymin" value="<?php echo $ymin; ?>"/>ymax=<input type="text" name="ymax" value="<?php echo $ymax; ?>"/><br />
+			<input type="submit" name="submit" />
 		</form>
 		<?php
 		echo '<img src="plot.php?expr='.urlencode($_REQUEST['func']).'&xmin='.$_REQUEST['xmin'].'&xmax='.$_REQUEST['xmax'].'&ymin='.$_REQUEST['ymin'].'&ymax='.$_REQUEST['ymax'].'" />';
