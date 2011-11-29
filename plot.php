4220bc6fb9b3fa0555910b986626f342c89824fe
Andreas VH <andreas.vinterhviid@gmail.com>
2011-11-29 15:58:23 +0100

    Add markers in x axis. Still needs some work
    Should be on screen even if axis is out of view.

--- plot.php
+++ plot.php
@@ -6,6 +6,16 @@ include_once('arithmetic.php');
 define("WIDTH", 500);
 define("HEIGHT", 500);
 
+$xmin = $_GET['xmin'];
+$xmax = $_GET['xmax'];
+$ymin = $_GET['ymin'];
+$ymax = $_GET['ymax'];
+$xrange = $xmax - $xmin;
+$yrange = $ymax - $ymin;
+//How many units does one pixel correspond to(on the x-axis)?:
+$xincr = $xrange / WIDTH;
+//How many units does one pixel correspond to(on the y-axis)?:
+$yincr = $yrange / HEIGHT;
 
 function cy($y) {
 	return HEIGHT - $y;
@@ -13,6 +23,49 @@ function cy($y) {
 function drawLine($img, $x1, $y1, $x2, $y2, $color) {
 	imageline($img, $x1, cy($y1), $x2, cy($y2), $color);
 }
+
+//http://stackoverflow.com/questions/7725278/round-to-nearest-nice-number
+//deep magic begins here
+function niceNumber($num) {
+	//the next power of 10 above num
+	$npw = pow(10, ceil(log10($num)));
+	//now we check were in the interval from 0 til our power num lies.
+	//it can never be lower than (1/10). So we check (2..3..4..5.. /10)
+	for($i = 2; $i < 10; ++$i) {
+		$toCheck = $npw * ($i/10);
+		if($num <= $toCheck) {
+			//we now have a (n/10) of the next power of 10 above our num, wich is what we wanted
+			return $toCheck;
+		}
+	}
+	return $npw;
+}
+
+function roundxInterval($pdist) {
+	global $xincr;
+	$udist = $pdist * $xincr; 
+	$rudist = niceNumber($udist);
+	return ($rudist / $xincr);
+}
+
+function drawXM($img, $dist, $y, $color) {
+	global $xincr;
+	global $xmin;
+	global $xmax;
+	$rpdist = roundxInterval($dist);
+	$rudist = niceNumber($dist * $xincr);
+	$yu = $y + 5;
+	$yl = $y - 5;
+	$x = 0;
+	$i = $xmin;
+	while($x <= WIDTH) {
+		drawLine($img, $x, $yl, $x, $yu, $color);
+		imagestring($img, 1, $x, cy($yl - 3), $i, $color);
+		$x += $rpdist;
+		$i += $rudist;
+	}
+}
+
 
 //Comment this line to show error messages.
 #header('Content-type: image/png');
@@ -27,22 +80,13 @@ if($mf->e()) {
 	imagestring($img, 3, 5, 5, $mf->m(), $blue);	
 } else {
 	$f = $mf->v();
-	$xmin = $_GET['xmin'];
-	$xmax = $_GET['xmax'];
-	$ymin = $_GET['ymin'];
-	$ymax = $_GET['ymax'];
-	$xrange = $xmax - $xmin;
-	$yrange = $ymax - $ymin;
-	//How many units does one pixel correspond to(on the x-axis)?:
-	$xincr = $xrange / WIDTH;
-	//How many units does one pixel correspond to(on the y-axis)?:
-	$yincr = $yrange / HEIGHT;
 
 
 	$yzero = (0-$ymin) / $yincr;
 	$xzero = (0-$xmin) / $xincr;
 	drawLine($img, $xzero, 0, $xzero, HEIGHT, $white);
 	drawLine($img, 0, $yzero, WIDTH, $yzero, $white);
+	drawXM($img, 20, $yzero, $white);
 
 	$lasty = Maybe::error("First last must be an error");
 	for($i = 0; $i <= WIDTH; $i += 1) {
